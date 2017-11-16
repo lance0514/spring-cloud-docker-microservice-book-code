b5d6bd02f55b8a07224017c62fef12ca444ec28e
pengwao@163.com <pengwao@163.com>
2017-11-16 20:01:04 +0800

    11/16

---
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/Controller/MoiveController.java
@@ -0,0 +1,24 @@
+package com.itmuch.cloud.study.Controller;
+
+import com.itmuch.cloud.study.POJO.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MoiveController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${user.userServiceUrl}")
+    private String userServiceUrl;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable("id") long id) {
+        return restTemplate.getForObject(userServiceUrl + id, User.class);
+    }
+
+}
