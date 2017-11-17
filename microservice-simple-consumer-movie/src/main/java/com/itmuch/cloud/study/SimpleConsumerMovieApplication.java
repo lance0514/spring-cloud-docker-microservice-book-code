098c247e439331d18ba031a89ce09cc7c49ba39d
pengwao@163.com <pengwao@163.com>
2017-11-17 11:15:25 +0800

    rename

--- microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/SimpleConsumerMovieApplication.java
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/study/SimpleConsumerMovieApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class WebApplication {
+public class SimpleConsumerMovieApplication {
 
 
     @Bean
@@ -16,6 +16,6 @@ public class WebApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        SpringApplication.run(SimpleConsumerMovieApplication.class, args);
     }
 }
